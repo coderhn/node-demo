f11fa68050df3d135b6ce620feeb212c49eb9b7c
yuhuan.wang <yuhuan.wang@ly.com>
2024-08-14 18:22:22 +0800

    feat: 初始化

---
+++ index.js
@@ -0,0 +1,9 @@
+var server = require('./lib/server');
+var router = require('./lib/router');
+var requestHandlers = require("./requestHandlers");
+var handle = {}
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+
+server.start(router.route, handle);
